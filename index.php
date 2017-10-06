8aea4cf43c33ca45cc736d02afe6524cba315f46
gulzar3594 <32568995+gulzar3594@users.noreply.github.com>
2017-10-06 16:54:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Find Your Perfect Partner - Make My Love | Home :: 
+<title>Find Your Perfect Rishta - Swadeshi Rishta | Home :: 
 </title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
